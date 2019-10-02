1d49d5fbd0a61912bd7c61d13bca49338dbe1c7a
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-02 08:05:01 +0200

    wip

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -16,14 +16,17 @@ export const useLocalStorage = (key, initialValue) => {
 
 export const useDarkMode = () => {
   const [isDark, setIsdark] = useLocalStorage('dark', false);
+
   const toggleDark = () => {
     document.body.classList.toggle('dark-mode')
     setIsdark(!isDark);
   }
+
   useEffect(() => {
     isDark
       ? document.body.classList.add('dark-mode')
       : document.body.classList.remove('dark-mode');
   }, [isDark])
+
   return [isDark, toggleDark];
 }
